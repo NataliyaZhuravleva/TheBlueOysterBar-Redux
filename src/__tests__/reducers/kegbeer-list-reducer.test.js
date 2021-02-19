860aac27d0e69788acacb4edbd4e9f4af290db73
Nataliya Zhuravleva <natalindria@gmail.com>
2021-02-19 09:02:54 -0800

    Test add kegbeer purposefully fail

--- src/__tests__/reducers/kegbeer-list-reducer.test.js
+++ src/__tests__/reducers/kegbeer-list-reducer.test.js
@@ -1,7 +1,37 @@
 import kegBeerListReducer from '../../reducers/kegbeer-list-reducer';
 
 describe('kegBeerListReducer', () => {
+  let action;
+  const kegBeerData = {
+    name: 'Guinness Blonde',
+    brand: 'Guinness',
+    price: '6.99',
+    alcoholContent: '5.0',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegBeerListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg beer data to masterKegBeerList', () => {
+    const { name, brand, price, alcoholContent, id } = kegBeerData;
+    action = {
+      type: 'ADD_KEGBEER',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id
+    };
+    expect(kegBeerListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
   });
 });
