d74da562cc2e798ecd8c43351be78552a12d04cf
Nataliya Zhuravleva <natalindria@gmail.com>
2021-02-19 09:17:09 -0800

    Test to delete a kegbeer purposefully fail

--- src/__tests__/reducers/kegbeer-list-reducer.test.js
+++ src/__tests__/reducers/kegbeer-list-reducer.test.js
@@ -5,9 +5,25 @@ describe('kegBeerListReducer', () => {
   const kegBeerData = {
     name: 'Guinness Blonde',
     brand: 'Guinness',
-    price: 6.99,
+    price: 4.99,
     alcoholContent: 5.0,
     id: 1
+  }
+  const currentState = {
+    1: {
+      name: 'Guinness Blonde',
+      brand: 'Guinness',
+      price: 4.99,
+      alcoholContent: 5.0,
+      id: 1
+    },
+    2: {
+      name: 'Guinness Extra Stout',
+      brand: 'Guinness',
+      price: 5.99,
+      alcoholContent: 5.6,
+      id: 2
+    },
   }
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -32,6 +48,22 @@ describe('kegBeerListReducer', () => {
         alcoholContent: alcoholContent,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a keg beer', () => {
+    action = {
+      type: 'DELETE_KEGBEER',
+      id: 1
+    };
+    expect(kegBeerListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'Guinness Extra Stout',
+        brand: 'Guinness',
+        price: 5.99,
+        alcoholContent: 5.6,
+        id: 2
+      },
     });
   });
 });
