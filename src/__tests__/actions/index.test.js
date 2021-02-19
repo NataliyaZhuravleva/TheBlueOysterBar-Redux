961b43805215ee0bb3d28c9572899345a111efe4
Nataliya Zhuravleva <natalindria@gmail.com>
2021-02-19 11:56:56 -0800

    Add test for action ADD_KEGBEER

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,7 +1,8 @@
 import * as actions from './../../actions';
 
-describe ('the blue oyster bar actions', ()=>{
-  it('deleteKegBeer should create DELETE_KEGBEER action', ()=>{
+describe('the blue oyster bar actions', () => {
+
+  it('deleteKegBeer should create DELETE_KEGBEER action', () => {
     expect(actions.deleteKegBeer(1)).toEqual({
       type: 'DELETE_KEGBEER',
       id: 1
@@ -13,4 +14,15 @@ describe ('the blue oyster bar actions', ()=>{
       type: 'TOGGLE_FORM'
     });
   });
+
+  it('addKegBeer should create ADD_KEGBEER action', () => {
+    expect(actions.addKegBeer({ name: 'Guinness Blonde', brand: 'Guinness', price: 4.99, alcoholContent: 5.0, id: 1 })).toEqual({
+      type: 'ADD_KEGBEER',
+      name: 'Guinness Blonde',
+      brand: 'Guinness',
+      price: 4.99,
+      alcoholContent: 5.0,
+      id: 1
+    })
+  })
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,15 @@ export const deleteKegBeer = id =>({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addKegBeer = (kegBeer) => {
+  const { name, brand, price, alcoholContent, id } = kegBeer;
+  return {
+    type: 'ADD_KEGBEER',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id
+  }
+}
